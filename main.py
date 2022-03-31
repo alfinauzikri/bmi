4331d55684f8a42709e7dc629a1e41f4806a086a
Alfin Auzikri <iyalfin@gmail.com>
2022-03-31 09:02:05 +0700

    Update sesuai ketentuan soal dan optimasi kode

--- main.py
+++ main.py
@@ -6,31 +6,24 @@ app = Flask(__name__)
 @app.route('/api/v1/bmi', methods=['POST'])
 def bmi():
 
-    req = request.json
-
     height = float(request.form.get('height'))
     weight = float(request.form.get('weight'))
 
     BMI = weight / (height/100)**2
 
-    msg = "You BMI is " + str(BMI)
+    msg = "BMI Anda adalah " + str(BMI)
 
     if BMI <= 18.4:
-        ket = "You are underweight."
-    elif BMI <= 24.9:
-        ket = "You are healthy."
-    elif BMI <= 29.9:
-        ket = "You are over weight."
-    elif BMI <= 34.9:
-        ket = "You are severely over weight."
-    elif BMI <= 39.9:
-        ket = "You are obese."
+        ket = "Anda Kurus."
+    elif BMI <= 25:
+        ket = "Anda Normal."
+    elif BMI <= 40:
+        ket = "Berlebih."
     else:
-        ket = "You are severely obese."
+        ket = "Anda Dangerous."
 
     data = {'result': 'true', 'msg': msg, 'ket': ket}
-    return data
-
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
